dd7a13bbd042cf124f220e5cd87a0e1784b12152
Remgagagali727 <remgagagali@outlook.com>
2023-12-27 22:32:35 -0600

    Problem 1 and 2 solved

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,3 +1,37 @@
+
+//Fibonacci Problem
+//Recursive Programming
+//Memorization
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Problem1 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give me a number:");
+        int n = in.nextInt();
+        System.out.println("The fib output of "+n+ " is "+fib(n));
+
+    }
+
+    private static int fib(int n){
+        return fibhelper(n, new HashMap<Integer, Integer>());
+    }
+
+    public static int fibhelper(int n, HashMap<Integer, Integer> memo){
+        //Base cases of fibonacci
+        if(n==0 || n==1) return n;
+
+        //Checks if we have the fibonacci value saved and returns it
+        if(memo.containsKey(n)) return memo.get(n);
+
+        //Gets and saves the fibonacci value of n
+        int fibr = fibhelper(n-1, memo) + fibhelper(n-2, memo);
+        memo.put(n,fibr);
+
+        return fibr;
+    }
 }
---
+++ src/main/java/Problem2.java
@@ -0,0 +1,32 @@
+
+//Tribonacci Problem
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give me a number:");
+        int n = in.nextInt();
+        System.out.println("The fib output of "+n+ " is "+tib(n));
+    }
+
+    public static int tib(int n) {
+        return tibh(n,new HashMap<Integer,Integer>());
+    }
+
+    //Tribonacci Helper Class
+    private static int tibh(int n, HashMap<Integer, Integer> dic) {
+        //Base cases
+        if(n==0||n==1) return 0;
+        if(n==2) return 1;
+
+        //Returns the tribonacci value of n if its memorized
+        if(dic.containsKey(n)) return dic.get(n);
+
+        return tibh(n-1,dic)+tibh(n-2,dic)+tibh(n-3,dic);
+
+    }
+
+}
